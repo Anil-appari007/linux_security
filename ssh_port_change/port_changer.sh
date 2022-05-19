3259335010d5527c8ecd350191bd54611be11401
Anil-appari007 <80904240+Anil-appari007@users.noreply.github.com>
2022-05-19 11:58:37 +0530

    Create port_changer.sh

---
+++ ssh_port_change/port_changer.sh
@@ -0,0 +1,22 @@
+###Change the port number as you required"
+new_port_number_for_ssh=9111
+
+sshd_config='/etc/ssh/sshd_config'
+
+############
+##  Don't edit the below code
+##  set -x
+
+new_word="Port $new_port_number_for_ssh"
+
+existing_port=$(cat $sshd_config | grep -e "Port" | awk 'FNR==1 {print}')
+
+echo This is the existing  $existing_port. It will be changed to $new_port_number_for_ssh
+
+sudo sed -i "s/$existing_port/$new_word/g" $sshd_config
+cat $sshd_config | grep -e "Port"
+
+# remove comment if need to restart ssh service
+sudo service sshd restart
+
+service sshd status
